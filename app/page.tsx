acb338c67cbcdb2458ed59da06caaf938b18987f
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-07 16:17:03 +0900

    feat: create navigation bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,28 @@
 
 import React, { useState, useEffect } from "react";
 import Navbar from "@/components/Navbar";
+import Carousel from "@/components/Carousel";
+import Head from "next/head";
 
 export default function Home() {
   const [showShadow, setShowShadow] = useState<boolean>(false);
 
+  useEffect(() => {
+    const handleShadow = () => {
+        if (window.scrollY >= 90) {
+            setShowShadow(true);
+        }
+        else setShowShadow(false);
+    };
+    window.addEventListener('scroll', handleShadow);
+},[]);
+
   return (
-    <Navbar showShadow={showShadow} setShowShadow={setShowShadow}/>
+    <div className='w-full h-full'>
+      <div className='w-full h-full flex justify-center'>
+        <Navbar showShadow={showShadow} setShowShadow={setShowShadow}/>
+        <Carousel/>
+      </div>
+    </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,8 +11,8 @@ interface NavBarProps {
 const Navbar: React.FC<NavBarProps> = ({ showShadow, setShowShadow}) => {
 
   return (
-    <div className='fixed w-full h-32 z-[100] bg-white shadow-2xl'>
-        <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
+    <div className={showShadow ? "fixed top-0 left-0 right-0 z-[100] bg-white shadow" : "fixed top-0 left-0 right-0 z-[100] bg-white"}>
+        <div className="max-w-[1240px] mx-auto flex justify-between items-center p-2 2xl:px-16">
             <Link className='scroll-false' href='/'>
                 <Image
                     src={cvic_logo_600}
@@ -21,9 +21,20 @@ const Navbar: React.FC<NavBarProps> = ({ showShadow, setShowShadow}) => {
                     unoptimized
                 />
             </Link>
+            <div>language toggle</div>
+        </div>
+        <div className='max-w-[1240px] mx-auto flex justify-between items-center p-2 2xl:px-16"'>
+            <div className='w-full h-full p-2 grid grid-cols-7'>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Home</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Cardiac Imaging</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Inspection Flow</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Dock List</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Fees</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>Access</div>
+                <div className='p-2 bg-white text-md tracking-wide uppercase'>More</div>
+            </div>
         </div>
     </div>
-
   )
 }
 
