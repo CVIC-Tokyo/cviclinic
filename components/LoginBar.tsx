84354c6c13cb2d3f2a957f7c54df9c01ac879333
EdjannCuevas <edjanncuevas@icloud.com>
2024-03-11 19:16:44 +0900

    feat: initialize login bar

--- components/LoginBar.tsx
+++ components/LoginBar.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { AiOutlineLogin } from 'react-icons/ai'
 
 const LoginBar = () => {
   return (
     <div className='w-full h-full flex justify-center items-center p-2'>
-        <div className='w-[240px] bg-[#820000]'> Member Login Bar</div>
+        <div className='w-[240px] bg-[#820000] flex-row justify-center items-center'>
+          (Member Login Bar)*
+        </div>
     </div>
   )
 }
