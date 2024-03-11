6138102e91d35bffe03afbf45afa24f6cb37771a
EdjannCuevas <edjanncuevas@icloud.com>
2024-03-11 16:35:26 +0900

    feat: initialize log in bar component

---
+++ components/LoginBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginBar = () => {
+  return (
+    <div className='w-full h-full flex justify-center items-center p-2'>
+        <div className='w-[240px] bg-[#820000]'> Member Login Bar</div>
+    </div>
+  )
+}
+
+export default LoginBar
