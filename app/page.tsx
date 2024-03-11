eb8843ce6a06bacbeb99e976b0430e8ac3c4907a
EdjannCuevas <edjanncuevas@icloud.com>
2024-03-11 16:37:18 +0900

    feat: initialize log in bar component

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import Carousel from "@/components/Carousel";
 import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import LoginBar from "@/components/LoginBar";
 
 export default function Home() {
 
   return (
-  <div className='w-full h-auto'>
+  <div className='w-full h-auto pt-36 md:pt-40 lg:pt-52'>
+    <LoginBar/>
     <Carousel/>
-    <Link href='/about'>about</Link>
   </div>
 
   );
