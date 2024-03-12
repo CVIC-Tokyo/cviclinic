1bd14abf765e760d5fee76db32ec217d1985f05a
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-12 16:48:47 +0900

    feat: initialize doctors and equipments pages

---
+++ app/doctors/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DoctorsPage = () => {
+  return (
+    <div className="w-full h-auto">
+      <div className="w-full h-[600px] flex justify-center items-center">
+        Doctors' Page
+      </div>
+    </div>
+  );
+};
+
+export default DoctorsPage;
