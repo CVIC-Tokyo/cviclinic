3338223413c4f25ed17521275c595fe574df073d
Ed Cuevas <edjanncuevas@icloud.com>
2024-03-12 13:31:21 +0900

    feat: delete login component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Carousel from "@/app/components/Carousel";
-import LoginBar from "@/app/components/LoginBar";
 import './styles/globals.css'
 
 export default function Home() {
   return (
     <div className="w-full h-auto pt-36 md:pt-40 lg:pt-52">
-      <LoginBar />
       <Carousel />
     </div>
   );
